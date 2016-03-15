e898101bf97833f51904a8bd91e7451abe534832
Shinmera <shinmera@tymoon.eu>
2016-03-15 17:09:44 +0100

    Started work on server interface

--- server.lisp
+++ server.lisp
@@ -6,18 +6,72 @@
 
 (in-package #:org.shirakumo.north)
 
-(defvar *request-tokens* (make-hash-table :test 'equal))
-(defvar *access-tokens* (make-hash-table :test 'equal))
-(defvar *nonces* (make-hash-table :test 'equal))
+(defgeneric create-request-token-pair (server))
+(defgeneric create-access-token-pair (server))
+(defgeneric consumer-secret (server consumer-key))
+(defgeneric token-secret (server token))
+(defgeneric oauth/request-token (server request))
+(defgeneric oauth/authorize (server request))
+(defgeneric oauth/access-token (server request))
+(defgeneric oauth/verify (server request))
 
-;; (defun request-token (consumer-key signature-method signature timestamp nonce &key (version "1.0") (callback "oob"))
-;;   (values token token-secret callback-confirmed))
+(defclass server ()
+  ((nonces :initform (make-hash-table :test 'equal) :accessor nonces)))
 
-;; (defun authorize (token token-secret)
-;;   (values token verifier))
+(defmethod create-request-token-pair ((server server))
+  (values (make-nonce) (make-nonce)))
 
-;; (defun access-token (consumer-key token signature-method signature timestamp nonce verifier &key (version "1.0"))
-;;   (values token token-secret))
+(defmethod create-access-token-pair ((server server))
+  (values (make-nonce) (make-nonce)))
 
-;; (defun verify (consumer-key token signature-method signature timestamp nonce &key (version "1.0"))
-;;   )
+(defun check-parameters-present (request &rest parameters)
+  (loop for param in parameters
+        unless (pget param (oauth request))
+        collect param into missing
+        finally ;; FIXME
+        (when missing (error "Parameters ~s missing!" missing))))
+
+(defun check-request (request server)
+  (let ((consumer-key (pget :oauth_consumer_key (oauth request)))
+        (token-secret (pget :oauth_token (oauth request)))
+        (version (pget :oauth_version (oauth request)))
+        (nonce (pget :oauth_nonce (oauth request)))
+        (timestamp (pget :oauth_timestamp (oauth request))))
+    (if (find timestamp (gethash nonce (nonces server)) :test #'string=)
+        ;; FIXME
+        (error "Nonce reused.")
+        (push timestamp (gethash nonce (nonces server))))
+    (unless (or (not version) (string= "1.0" version))
+      ;; FIXME
+      (error "Bad version."))
+    (unless (verify request
+                    (consumer-secret server consumer-key)
+                    (token-secret server token))
+      ;; FIXME
+      (error "Signature failed!"))))
+
+(defmethod oauth/request-token ((server server) (request request))
+  (check-parameters-present request
+    :oauth_consumer_key :oauth_signature_method :oauth_signature
+    :oauth_timestamp :oauth_nonce :oauth_callback)
+  (check-request request server)
+  (multiple-value-bind (request-token token-secret) (create-request-token-pair server)
+    (values request-token token-secret T)))
+
+(defmethod oauth/authorize ((server server) (request request))
+  (check-parameters-present request :oauth_token)
+  (values token verifier))
+
+(defmethod oauth/access-token ((server server) (request request))
+  (check-parameters-present request
+    :oauth_consumer_key :oauth_signature_method :oauth_signature
+    :oauth_timestamp :oauth_nonce :oauth_token :oauth-verifier)
+  (check-request request server)
+  (values token token-secret))
+
+(defmethod oauth/verify ((server server) (request request))
+  (check-parameters-present request
+    :oauth_consumer_key :oauth_signature_method :oauth_signature
+    :oauth_timestamp :oauth_nonce :oauth_token)
+  (check-request request server)
+  T)
